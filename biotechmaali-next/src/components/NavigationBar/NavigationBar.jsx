f30a7cf21253128ff32c19cf1ae8b7ba2b4aa9b4
apniprogrammingyt09 <bhagatkrish65@gmail.com>
2026-02-20 05:04:22 +0530

    fix: comment out stray usePathname() call outside function body in NavigationBar

--- biotechmaali-next/src/components/NavigationBar/NavigationBar.jsx
+++ biotechmaali-next/src/components/NavigationBar/NavigationBar.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter, useSearchParams, usePathname } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import { FaRegUser, FaRegHeart, FaChevronDown } from "react-icons/fa";
@@ -415,7 +415,7 @@ export default NavBar;
 //
 //
 //   const searchParams = useSearchParams();
-  const pathname = usePathname();
+//   const pathname = usePathname();
 //   useEffect(() => {
 //     const params = new URLSearchParams(`?${searchParams.toString()}`);
 //     const modal = params.get("modal"); // Check query parameter 'modal'
