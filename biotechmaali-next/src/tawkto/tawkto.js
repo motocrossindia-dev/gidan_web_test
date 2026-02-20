0e1f9da7884cf2e78f84739ef5dcc518b57760cb
apniprogrammingyt09 <bhagatkrish65@gmail.com>
2026-02-20 06:38:09 +0530

    fix: remove crossorigin='*' from Tawk.to script - caused CORS failures on widget-settings, session/start and language files

--- biotechmaali-next/src/tawkto/tawkto.js
+++ biotechmaali-next/src/tawkto/tawkto.js
@@ -15,7 +15,6 @@ const TawkToWidget = () => {
             s1.async=true;
             s1.src='https://embed.tawk.to/671a6b3a2480f5b4f593433b/1iavj5o5a';
             s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);
           })();
         `,
