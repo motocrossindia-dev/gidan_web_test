13ae33c14e1016ddbd9b8bdd1a50fa0c7bc11915
Hawk <hawk@Hawks-MacBook-Air.local>
2026-02-26 18:08:30 +0530

    push .env

--- src/views/utilities/seo/HomepageSchema.jsx
+++ src/views/utilities/seo/HomepageSchema.jsx
@@ -1,15 +1,17 @@
 export default function HomepageSchema({
     siteUrl = "https://www.gidan.store/",
 }) {
+    const cleanSiteUrl = siteUrl.replace(/\/$/, "");
+
     const websiteSchema = {
         "@context": "https://schema.org",
         "@type": "WebSite",
-        "@id": `${siteUrl.replace(/\/$/, '')}/#website`,
-        "url": siteUrl,
+        "@id": `${cleanSiteUrl}/#website`,
+        "url": cleanSiteUrl,
         "name": "Gidan Store",
         "potentialAction": {
             "@type": "SearchAction",
-            "target": `${siteUrl}search?&q={query}`,
+            "target": `${cleanSiteUrl}/search?&q={query}`,
             "query": "required"
         }
     };
@@ -17,16 +19,47 @@ export default function HomepageSchema({
     const webpageSchema = {
         "@context": "https://schema.org",
         "@type": "WebPage",
-        "@id": `${siteUrl.replace(/\/$/, '')}/#webpage`,
-        "url": siteUrl,
+        "@id": `${cleanSiteUrl}/#webpage`,
+        "url": cleanSiteUrl,
         "name": "Home - Gidan Store",
         "isPartOf": {
-            "@id": `${siteUrl.replace(/\/$/, '')}/#website`
+            "@id": `${cleanSiteUrl}/#website`
         },
         "about": {
-            "@id": `${siteUrl.replace(/\/$/, '')}/#organization`
+            "@id": `${cleanSiteUrl}/#organization`
         },
         "description": "Explore Gidan Store's collection of plants, planters, and gardening essentials for your urban garden."
+    };
+
+    const organizationSchema = {
+        "@context": {
+            "@vocab": "https://schema.org/",
+            "gs1": "https://ref.gs1.org/voc/",
+            "unece": "https://vocabulary.uncefact.org/"
+        },
+        "@type": [
+            "Organization",
+            "gs1:Organization",
+            "unece:TradeParty"
+        ],
+        "@id": `${cleanSiteUrl}/#organization`,
+        "name": "Gidan Store",
+        "url": cleanSiteUrl,
+        "logo": `${cleanSiteUrl}/logo192.ico`,
+        "description": "Bangalore's trusted destination for plants, planters, and urban gardening essentials.",
+        "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Bangalore",
+            "addressRegion": "Karnataka",
+            "postalCode": "560001",
+            "addressCountry": "IN"
+        },
+        "sameAs": [
+            "https://www.facebook.com/thegidanstore/",
+            "https://www.instagram.com/thegidanstore/",
+            "https://www.linkedin.com/company/thegidanstore/",
+            "https://www.youtube.com/@thegidanstore/"
+        ]
     };
 
     return (
@@ -38,6 +71,10 @@ export default function HomepageSchema({
             <script
                 type="application/ld+json"
                 dangerouslySetInnerHTML={{ __html: JSON.stringify(webpageSchema) }}
+            />
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
             />
         </>
     );
